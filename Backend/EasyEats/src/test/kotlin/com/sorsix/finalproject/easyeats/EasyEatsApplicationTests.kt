91a094b0ed9334c97da1da6a4d0919ad89c283d7
matejapetkovska <matejapetkovska2@gmail.com>
2023-07-18 13:28:33 +0200

    Initial commit

---
+++ Backend/EasyEats/src/test/kotlin/com/sorsix/finalproject/easyeats/EasyEatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sorsix.finalproject.easyeats
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasyEatsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
