8c26a8e278c2ff59812c2d62d8db0fb548acdefc
matejapetkovska <matejapetkovska2@gmail.com>
2023-07-18 18:41:25 +0200

    Refactoring project files

---
+++ EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/EasyEatsApplication.kt
@@ -0,0 +1,11 @@
+package com.sorsix.finalproject.easyeats
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyEatsApplication
+
+fun main(args: Array<String>) {
+    runApplication<EasyEatsApplication>(*args)
+}
