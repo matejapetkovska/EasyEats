ab0967638575f5b63c7bc4fac3ce3c7656a8b6ee
Ivana Dimitrovska <ivana.z.dimitrovska@gmail.com>
2023-07-19 13:20:07 +0200

    Changes

--- EasyEats/build.gradle.kts
+++ EasyEats/build.gradle.kts
@@ -30,28 +30,7 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.rest-assured:rest-assured:5.3.1")
-    //implementation("org.hibernate:hibernate-core:1.5.30") // Replace with the desired Hibernate version
-    //implementation("org.hibernate:hibernate-entitymanager:1.5.30") // Replace with the desired Hibernate version
 }
-
-//application {
-//    // Application configuration
-//    mainClassName = "your.main.class.package.MainClass" // Replace with your main class package and name
-//}
-//
-//// Hibernate configuration
-//configurations {
-//    hibernateDialect {
-//        description = "The Hibernate dialect for PostgreSQL 9.5"
-//        resolutionStrategy {
-//            eachDependency {
-//                if (requested.group == "org.hibernate" && requested.name.startsWith("hibernate-core")) {
-//                    useVersion("x.x.x") // Replace with the desired Hibernate version
-//                }
-//            }
-//        }
-//    }
-//}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
