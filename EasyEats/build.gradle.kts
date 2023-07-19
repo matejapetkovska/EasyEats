0d1bc266d9be61a61be6a53912aaa3d3df47c0f3
Ivana Dimitrovska <ivana.z.dimitrovska@gmail.com>
2023-07-19 13:10:45 +0200

    New Commit

--- EasyEats/build.gradle.kts
+++ EasyEats/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.8.22"
 }
 
 group = "com.sorsix.finalproject"
@@ -26,8 +27,31 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.1.1")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.rest-assured:rest-assured:5.3.1")
+    //implementation("org.hibernate:hibernate-core:1.5.30") // Replace with the desired Hibernate version
+    //implementation("org.hibernate:hibernate-entitymanager:1.5.30") // Replace with the desired Hibernate version
 }
+
+//application {
+//    // Application configuration
+//    mainClassName = "your.main.class.package.MainClass" // Replace with your main class package and name
+//}
+//
+//// Hibernate configuration
+//configurations {
+//    hibernateDialect {
+//        description = "The Hibernate dialect for PostgreSQL 9.5"
+//        resolutionStrategy {
+//            eachDependency {
+//                if (requested.group == "org.hibernate" && requested.name.startsWith("hibernate-core")) {
+//                    useVersion("x.x.x") // Replace with the desired Hibernate version
+//                }
+//            }
+//        }
+//    }
+//}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
