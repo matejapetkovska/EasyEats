2b1172159a036d4d1d6a5561c91f9a3d19bfbb9a
matejapetkovska <matejapetkovska2@gmail.com>
2023-07-20 12:54:51 +0200

    New db migration

---
+++ EasyEats/src/main/resources/db/migration/V1_add_initial_data_User_Review.sql
@@ -0,0 +1,5 @@
+insert into users(email,first_name,image,last_name,password,role,username)
+values ('test@test.com','Mateja','https://cdn.icon-icons.com/icons2/2643/PNG/512/avatar_female_woman_person_people_white_tone_icon_159360.png','Petkovska','test1234','USER','mateja123');
+
+insert into reviews(comment, rating, recipe_id, user_id)
+values ('Very easy recipe',5,1,1)
