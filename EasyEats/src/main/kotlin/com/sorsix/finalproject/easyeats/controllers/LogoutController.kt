5b45901be4fbc7945c20aa9875af6135b96cb6ee
matejapetkovska <matejapetkovska2@gmail.com>
2023-07-25 10:47:55 +0200

    Login interface

--- EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/controllers/LogoutController.kt
+++ EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/controllers/LogoutController.kt
@@ -1,18 +1,9 @@
 package com.sorsix.finalproject.easyeats.controllers
 
-import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.servlet.view.RedirectView
 
 @Controller
 @RequestMapping("/logout")
 class LogoutController {
-
-    @GetMapping
-    fun logout(request:HttpServletRequest): RedirectView {
-        request.session.invalidate()
-        return RedirectView("/api/login")
-    }
 }
