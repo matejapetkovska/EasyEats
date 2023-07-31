3e9ff7b6b52ebc88e6324ee166f37a40750e4e29
Ivana Dimitrovska <ivana.z.dimitrovska@gmail.com>
2023-07-31 21:57:31 +0200

    new commit

--- EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/controllers/RecipesController.kt
+++ EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/controllers/RecipesController.kt
@@ -1,65 +1,65 @@
-package com.sorsix.finalproject.easyeats.controllers
-
-import com.sorsix.finalproject.easyeats.models.Recipe
-import com.sorsix.finalproject.easyeats.service.RecipeService
-import com.sorsix.finalproject.easyeats.service.UserService
-import jakarta.servlet.http.HttpServletRequest
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
-
-@RestController
-@RequestMapping("/recipes")
-@CrossOrigin(origins = ["http://localhost:4200"])
-class RecipesController(private val recipeService: RecipeService,
-                        private val userService: UserService) {
-    @GetMapping("/{category_id}")
-    fun getRecipesByCategory(@PathVariable category_id: String) : List<Recipe>? {
-        return recipeService.getAllRecipesByCategory(category_id)
-    }
-
-    @GetMapping("/{category_id}/{subCategory_id}")
-    fun getRecipesByCategoryAndSubCategory(@PathVariable category_id: String,
-                                           @PathVariable subCategory_id: String): List<Recipe>?{
-        return recipeService.getAllRecipesByCategoryAndSubCategory(category_id, subCategory_id)
-    }
-
-    @GetMapping("/")
-    fun getAllRecipes(): List<Recipe> {
-        return recipeService.getAllRecipes()
-    }
-
-    @GetMapping("/query/{queryText}")
-    fun getRecipesByTitleOrDescriptionContainingQueryText(@PathVariable queryText: String): List<Recipe>{
-        return recipeService.getAllRecipesByTitleContaining(queryText)
-    }
-
-    @PostMapping("/add")
-    fun addRecipe(@RequestParam title: String,
-                  @RequestParam description: String,
-                  @RequestParam file: MultipartFile,
-                  @RequestParam category_id: String,
-                  @RequestParam subCategory_id: String,
-                  @RequestParam ingredients: String,
-                  request: HttpServletRequest): ResponseEntity<Any>{
-        val user = userService.getLoggedInUser(request)
-            ?: return ResponseEntity.badRequest().body(Error("Error in saving recipe. Please log in first."))
-        val recipe = recipeService.addRecipe(title, description, file, category_id, subCategory_id, ingredients, user)
-        return ResponseEntity.ok(recipe)
-    }
-
-    @PutMapping("/edit/{recipe_id}")
-    fun editRecipe(@PathVariable recipe_id: String,
-                   @RequestParam title: String,
-                   @RequestParam description: String,
-                   @RequestParam category_id: String,
-                   @RequestParam subCategory_id: String,
-                   @RequestParam ingredients: String,
-                   request: HttpServletRequest) : ResponseEntity<Any> {
-        val user = userService.getLoggedInUser(request)
-            ?: return ResponseEntity.badRequest().body(Error("Error in editing recipe. Please log in first."))
-        val recipe = recipeService.editRecipe(recipe_id,title, description, category_id, subCategory_id, ingredients, user)
-            ?: return ResponseEntity.badRequest().body(Error("Error in editing recipe."))
-        return ResponseEntity.ok(recipe)
-    }
-}
+//package com.sorsix.finalproject.easyeats.controllers
+//
+//import com.sorsix.finalproject.easyeats.models.Recipe
+//import com.sorsix.finalproject.easyeats.service.RecipeService
+//import com.sorsix.finalproject.easyeats.service.UserService
+//import jakarta.servlet.http.HttpServletRequest
+//import org.springframework.http.ResponseEntity
+//import org.springframework.web.bind.annotation.*
+//import org.springframework.web.multipart.MultipartFile
+//
+//@RestController
+//@RequestMapping("/recipes")
+//@CrossOrigin(origins = ["http://localhost:4200"])
+//class RecipesController(private val recipeService: RecipeService,
+//                        private val userService: UserService) {
+//    @GetMapping("/{category_id}")
+//    fun getRecipesByCategory(@PathVariable category_id: String) : List<Recipe>? {
+//        return recipeService.getAllRecipesByCategory(category_id)
+//    }
+//
+//    @GetMapping("/{category_id}/{subCategory_id}")
+//    fun getRecipesByCategoryAndSubCategory(@PathVariable category_id: String,
+//                                           @PathVariable subCategory_id: String): List<Recipe>?{
+//        return recipeService.getAllRecipesByCategoryAndSubCategory(category_id, subCategory_id)
+//    }
+//
+//    @GetMapping("/")
+//    fun getAllRecipes(): List<Recipe> {
+//        return recipeService.getAllRecipes()
+//    }
+//
+//    @GetMapping("/query/{queryText}")
+//    fun getRecipesByTitleOrDescriptionContainingQueryText(@PathVariable queryText: String): List<Recipe>{
+//        return recipeService.getAllRecipesByTitleContaining(queryText)
+//    }
+//
+//    @PostMapping("/add")
+//    fun addRecipe(@RequestParam title: String,
+//                  @RequestParam description: String,
+//                  @RequestParam file: MultipartFile,
+//                  @RequestParam category_id: String,
+//                  @RequestParam subCategory_id: String,
+//                  @RequestParam ingredients: String,
+//                  request: HttpServletRequest): ResponseEntity<Any>{
+//        val user = userService.getLoggedInUser(request)
+//            ?: return ResponseEntity.badRequest().body(Error("Error in saving recipe. Please log in first."))
+//        val recipe = recipeService.addRecipe(title, description, file, category_id, subCategory_id, ingredients, user)
+//        return ResponseEntity.ok(recipe)
+//    }
+//
+//    @PutMapping("/edit/{recipe_id}")
+//    fun editRecipe(@PathVariable recipe_id: String,
+//                   @RequestParam title: String,
+//                   @RequestParam description: String,
+//                   @RequestParam category_id: String,
+//                   @RequestParam subCategory_id: String,
+//                   @RequestParam ingredients: String,
+//                   request: HttpServletRequest) : ResponseEntity<Any> {
+//        val user = userService.getLoggedInUser(request)
+//            ?: return ResponseEntity.badRequest().body(Error("Error in editing recipe. Please log in first."))
+//        val recipe = recipeService.editRecipe(recipe_id,title, description, category_id, subCategory_id, ingredients, user)
+//            ?: return ResponseEntity.badRequest().body(Error("Error in editing recipe."))
+//        return ResponseEntity.ok(recipe)
+//    }
+//}
--- EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/service/implementations/RecipeServiceImpl.kt
+++ EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/service/implementations/RecipeServiceImpl.kt
@@ -1,174 +1,174 @@
-package com.sorsix.finalproject.easyeats.service.implementations
-
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.sorsix.finalproject.easyeats.models.Recipe
-import com.sorsix.finalproject.easyeats.models.User
-import com.sorsix.finalproject.easyeats.models.dto.IngredientDto
-import com.sorsix.finalproject.easyeats.repository.RecipeRepository
-import com.sorsix.finalproject.easyeats.service.*
-import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
-import java.time.LocalDateTime
-import kotlin.io.path.exists
-
-@Service
-class RecipeServiceImpl(
-    private val recipeRepository: RecipeRepository,
-    private val categoryService: CategoryService,
-    private val subCategoryService: SubCategoryService,
-    private val ingredientService: IngredientService
-) : RecipeService {
-    override fun getAllRecipesByCategory(category_id: String): List<Recipe>? {
-        val categoryIdLong = category_id.toLongOrNull()
-        if (categoryIdLong != null) {
-            return recipeRepository.findByCategory_Id(categoryIdLong)
-        }
-        return null
-    }
-
-    override fun getAllRecipesByCategoryAndSubCategory(
-        category_id: String,
-        subCategory_id: String
-    ): List<Recipe>? {
-        val categoryIdLong = category_id.toLongOrNull()
-        val subCategoryIdLong = subCategory_id.toLongOrNull()
-        if (categoryIdLong != null && subCategoryIdLong != null) {
-            return recipeRepository.findByCategory_IdAndSubCategory_Id(categoryIdLong, subCategoryIdLong)
-        }
-        return null
-    }
-
-    override fun getAllRecipes(): List<Recipe> {
-        return recipeRepository.findAll()
-    }
-
-    override fun getAllRecipesByTitleContaining(queryText: String): List<Recipe> {
-        return recipeRepository.findByTitleContainingIgnoreCase(queryText)
-    }
-    private fun generateRandomImageName(): String{
-        val sb = StringBuilder()
-        for (i in 0..5) {
-            val rand = listOf(('a'..'z'), ('A'..'Z')).flatten().random()
-            sb.append(rand)
-        }
-        return sb.toString()
-    }
-
-
-    override fun addRecipe(
-        title: String,
-        description: String,
-        file: MultipartFile,
-        category_id: String,
-        subCategory_id: String,
-        ingredients: String,
-        user: User
-    ): Recipe? {
-        val category = categoryService.getCategoryById(category_id.toLong())
-        val subCategory = subCategoryService.getSubCategoryById(subCategory_id.toLong())
-        val imageName = file.originalFilename
-        var recipe: Recipe? = null
-        if (imageName != null) {
-            val imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
-            if (!imageFilePath.exists()) {
-                Files.copy(file.inputStream, Paths.get(imageFilePath.toString()))
-                recipe = Recipe(
-                    0, title, description,
-                    mutableListOf(), imageName, LocalDateTime.now(),
-                    category, subCategory, user
-                )
-            }
-
-    override fun addRecipe( title: String,
-                            description: String,
-                            file: MultipartFile,
-                            category_id: String,
-                            subCategory_id: String,
-                            ingredients: String,
-                            user: User): Recipe {
-        val category = categoryService.getCategoryById(category_id.toLong())
-        val subCategory = subCategoryService.getSubCategoryById(subCategory_id.toLong())
-
-        var imageName = generateRandomImageName()+".jpg"
-        var imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
-        while(imageFilePath.exists()) {
-            imageName = generateRandomImageName()
-            imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
-        }
-        Files.copy(file.inputStream, Paths.get(imageFilePath.toString()))
-
-        val recipe = Recipe(
-                0, title, description,
-                mutableListOf(), imageName, LocalDateTime.now(),
-                category, subCategory, user
-            )
-            for (ingredient in ingredientsList) {
-                val savedIngredient =
-                    ingredientService.saveIngredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit)
-                recipe.ingredients.add(savedIngredient)
-            }
-            return recipeRepository.save(recipe)
-        }
-        return null
-    }
-
-    override fun editRecipe(
-        recipe_id: String,
-        title: String,
-        description: String,
-        category_id: String,
-        subCategory_id: String,
-        ingredients: String,
-        user: User
-    ): Recipe? {
-        val recipeIdLong = recipe_id.toLongOrNull()
-        var recipe = recipeIdLong?.let { recipeRepository.findById(it) }
-        if (recipe != null) {
-            recipe.get().title = title
-            recipe.get().description = description
-//            if (!file.isEmpty) {
-//                val imageName = file.originalFilename
-//                if (imageName != null) {
-//                    val imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
-//                    Files.copy(file.inputStream, Paths.get(imageFilePath.toString()), StandardCopyOption.REPLACE_EXISTING)
-//                    recipe.get().image = imageName
-//                }
+//package com.sorsix.finalproject.easyeats.service.implementations
+//
+//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+//import com.sorsix.finalproject.easyeats.models.Recipe
+//import com.sorsix.finalproject.easyeats.models.User
+//import com.sorsix.finalproject.easyeats.models.dto.IngredientDto
+//import com.sorsix.finalproject.easyeats.repository.RecipeRepository
+//import com.sorsix.finalproject.easyeats.service.*
+//import org.springframework.stereotype.Service
+//import org.springframework.web.multipart.MultipartFile
+//import java.io.File
+//import java.nio.file.Files
+//import java.nio.file.Paths
+//import java.nio.file.StandardCopyOption
+//import java.time.LocalDateTime
+//import kotlin.io.path.exists
+//
+//@Service
+//class RecipeServiceImpl(
+//    private val recipeRepository: RecipeRepository,
+//    private val categoryService: CategoryService,
+//    private val subCategoryService: SubCategoryService,
+//    private val ingredientService: IngredientService
+//) : RecipeService {
+//    override fun getAllRecipesByCategory(category_id: String): List<Recipe>? {
+//        val categoryIdLong = category_id.toLongOrNull()
+//        if (categoryIdLong != null) {
+//            return recipeRepository.findByCategory_Id(categoryIdLong)
+//        }
+//        return null
+//    }
+//
+//    override fun getAllRecipesByCategoryAndSubCategory(
+//        category_id: String,
+//        subCategory_id: String
+//    ): List<Recipe>? {
+//        val categoryIdLong = category_id.toLongOrNull()
+//        val subCategoryIdLong = subCategory_id.toLongOrNull()
+//        if (categoryIdLong != null && subCategoryIdLong != null) {
+//            return recipeRepository.findByCategory_IdAndSubCategory_Id(categoryIdLong, subCategoryIdLong)
+//        }
+//        return null
+//    }
+//
+//    override fun getAllRecipes(): List<Recipe> {
+//        return recipeRepository.findAll()
+//    }
+//
+//    override fun getAllRecipesByTitleContaining(queryText: String): List<Recipe> {
+//        return recipeRepository.findByTitleContainingIgnoreCase(queryText)
+//    }
+//    private fun generateRandomImageName(): String{
+//        val sb = StringBuilder()
+//        for (i in 0..5) {
+//            val rand = listOf(('a'..'z'), ('A'..'Z')).flatten().random()
+//            sb.append(rand)
+//        }
+//        return sb.toString()
+//    }
+//
+//
+//    override fun addRecipe(
+//        title: String,
+//        description: String,
+//        file: MultipartFile,
+//        category_id: String,
+//        subCategory_id: String,
+//        ingredients: String,
+//        user: User
+//    ): Recipe? {
+//        val category = categoryService.getCategoryById(category_id.toLong())
+//        val subCategory = subCategoryService.getSubCategoryById(subCategory_id.toLong())
+//        val imageName = file.originalFilename
+//        var recipe: Recipe? = null
+//        if (imageName != null) {
+//            val imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
+//            if (!imageFilePath.exists()) {
+//                Files.copy(file.inputStream, Paths.get(imageFilePath.toString()))
+//                recipe = Recipe(
+//                    0, title, description,
+//                    mutableListOf(), imageName, LocalDateTime.now(),
+//                    category, subCategory, user
+//                )
 //            }
-            val category = category_id.toLongOrNull()?.let { categoryService.getCategoryById(it) }
-            recipe.get().category = category
-            val subCategory = category_id.toLongOrNull()?.let { subCategoryService.getSubCategoryById(it) }
-            recipe.get().subCategory = subCategory
-
-            val objectMapper = jacksonObjectMapper()
-            val ingredientsList: List<IngredientDto> = objectMapper.readValue(
-                ingredients,
-                objectMapper.typeFactory.constructCollectionType(List::class.java, IngredientDto::class.java)
-            )
-
-            recipe.get().ingredients.clear()
-
-            for (ingredient in ingredientsList) {
-                val savedIngredient =
-                    ingredientService.saveIngredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit)
-                recipe.get().ingredients.add(savedIngredient)
-            }
-
-            recipeRepository.save(recipe.get())
-        }
-        return null
-
-        val objectMapper = jacksonObjectMapper()
-        val ingredientsList: List<IngredientDto> = objectMapper.readValue(ingredients,
-            objectMapper.typeFactory.constructCollectionType(List::class.java, IngredientDto::class.java))
-        for (ingredient in ingredientsList) {
-            val savedIngredient = ingredientService.saveIngredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit)
-            recipe.ingredients.add(savedIngredient)
-        }
-
-        return recipeRepository.save(recipe)
-    }
-}
+//
+//    override fun addRecipe( title: String,
+//                            description: String,
+//                            file: MultipartFile,
+//                            category_id: String,
+//                            subCategory_id: String,
+//                            ingredients: String,
+//                            user: User): Recipe {
+//        val category = categoryService.getCategoryById(category_id.toLong())
+//        val subCategory = subCategoryService.getSubCategoryById(subCategory_id.toLong())
+//
+//        var imageName = generateRandomImageName()+".jpg"
+//        var imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
+//        while(imageFilePath.exists()) {
+//            imageName = generateRandomImageName()
+//            imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
+//        }
+//        Files.copy(file.inputStream, Paths.get(imageFilePath.toString()))
+//
+//        val recipe = Recipe(
+//                0, title, description,
+//                mutableListOf(), imageName, LocalDateTime.now(),
+//                category, subCategory, user
+//            )
+//            for (ingredient in ingredientsList) {
+//                val savedIngredient =
+//                    ingredientService.saveIngredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit)
+//                recipe.ingredients.add(savedIngredient)
+//            }
+//            return recipeRepository.save(recipe)
+//        }
+//        return null
+//    }
+//
+//    override fun editRecipe(
+//        recipe_id: String,
+//        title: String,
+//        description: String,
+//        category_id: String,
+//        subCategory_id: String,
+//        ingredients: String,
+//        user: User
+//    ): Recipe? {
+//        val recipeIdLong = recipe_id.toLongOrNull()
+//        var recipe = recipeIdLong?.let { recipeRepository.findById(it) }
+//        if (recipe != null) {
+//            recipe.get().title = title
+//            recipe.get().description = description
+////            if (!file.isEmpty) {
+////                val imageName = file.originalFilename
+////                if (imageName != null) {
+////                    val imageFilePath = Paths.get("src\\main\\Frontend\\EasyEats\\src\\assets\\recipe_images\\", imageName)
+////                    Files.copy(file.inputStream, Paths.get(imageFilePath.toString()), StandardCopyOption.REPLACE_EXISTING)
+////                    recipe.get().image = imageName
+////                }
+////            }
+//            val category = category_id.toLongOrNull()?.let { categoryService.getCategoryById(it) }
+//            recipe.get().category = category
+//            val subCategory = category_id.toLongOrNull()?.let { subCategoryService.getSubCategoryById(it) }
+//            recipe.get().subCategory = subCategory
+//
+//            val objectMapper = jacksonObjectMapper()
+//            val ingredientsList: List<IngredientDto> = objectMapper.readValue(
+//                ingredients,
+//                objectMapper.typeFactory.constructCollectionType(List::class.java, IngredientDto::class.java)
+//            )
+//
+//            recipe.get().ingredients.clear()
+//
+//            for (ingredient in ingredientsList) {
+//                val savedIngredient =
+//                    ingredientService.saveIngredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit)
+//                recipe.get().ingredients.add(savedIngredient)
+//            }
+//
+//            recipeRepository.save(recipe.get())
+//        }
+//        return null
+//
+//        val objectMapper = jacksonObjectMapper()
+//        val ingredientsList: List<IngredientDto> = objectMapper.readValue(ingredients,
+//            objectMapper.typeFactory.constructCollectionType(List::class.java, IngredientDto::class.java))
+//        for (ingredient in ingredientsList) {
+//            val savedIngredient = ingredientService.saveIngredient(ingredient.name, ingredient.quantity, ingredient.measurementUnit)
+//            recipe.ingredients.add(savedIngredient)
+//        }
+//
+//        return recipeRepository.save(recipe)
+//    }
+//}
