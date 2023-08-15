e8aea189ef0cebc5265cca8741435fc937332027
matejapetkovska <matejapetkovska2@gmail.com>
2023-08-15 13:39:34 +0200

    Final changes

--- EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/EasyEatsApplication.kt
+++ EasyEats/src/main/kotlin/com/sorsix/finalproject/easyeats/EasyEatsApplication.kt
@@ -2,11 +2,6 @@ package com.sorsix.finalproject.easyeats
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.servlet.FilterRegistrationBean
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 
 @SpringBootApplication
